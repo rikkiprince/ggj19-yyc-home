eccc5ec8b620b9592f80217ce0be9ad7134622c3
Lee Davey <lee.davey@gmail.com>
2019-01-26 01:35:29 -0700

    Player is now a hexagon, added camera and background for effect and added power up.

--- godot-project/Player/Player.gd
+++ godot-project/Player/Player.gd
@@ -1,24 +1,19 @@
 extends Node2D
 
 const MOTION_SPEED = 8000
+#var JOYPAD_SENSITIVITY = 5
+const JOYPAD_DEADZONE = 0.30
 
-func _ready():
-	pass
+#func _ready():
+#	pass
 
 func _physics_process(delta):
-	var motion = Vector2()
-	if Input.get_joy_axis(0,
-	if Input.is_action_pressed("ui_up"):
-		motion += Vector2(0, -1)
-	if Input.is_action_pressed("ui_down"):
-		motion += Vector2(0, 1)
-	if Input.is_action_pressed("ui_left"):
-		motion += Vector2(-1, 0)
-	if Input.is_action_pressed("ui_right"):
-		motion += Vector2(1, 0)
-	if Input.is_action_pressed("ui_accept"):
-		print("button A")
-	if Input.is_action_pressed("ui_cancel"):
-		print("button B")
-	motion = motion.normalized() * MOTION_SPEED * delta
-	get_node("KinematicBody2D").move_and_slide(motion)
+	var joypad_vec = Vector2(0, 0)
+	
+	joypad_vec = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
+	
+	if joypad_vec.length() < JOYPAD_DEADZONE:
+		joypad_vec = Vector2(0, 0)
+	else:
+		joypad_vec = joypad_vec.normalized() * ((joypad_vec.length() - JOYPAD_DEADZONE) / (1 - JOYPAD_DEADZONE))
+	get_node("KinematicBody2D").move_and_slide(joypad_vec*delta*MOTION_SPEED)
