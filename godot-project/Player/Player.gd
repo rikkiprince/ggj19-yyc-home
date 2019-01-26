13eaf82eda1095acf809f430b12ab1497070f2db
Konrad Wisniewski <konrad.m.wisniewski@gmail.com>
2019-01-26 14:54:03 -0700

    Added Stamina and game end on 0 stamina
    Added Stamina and game end on 0 stamina

--- godot-project/Player/Player.gd
+++ godot-project/Player/Player.gd
@@ -4,9 +4,12 @@ const MOTION_SPEED = 8000
 #var JOYPAD_SENSITIVITY = 5
 const JOYPAD_DEADZONE = 0.50
 var joypad_vec
+var stamina
+
 
 func _ready():
 	joypad_vec = Vector2(0,0)
+	stamina = 100
 
 func _physics_process(delta):
 	pass
