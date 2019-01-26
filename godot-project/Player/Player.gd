2b7c402f8efba37e24d541d48ed8b7827ee84b4a
Lee Davey <lee.davey@gmail.com>
2019-01-26 02:49:21 -0700

    Adding an ambient soundtrack and constant player motion

--- godot-project/Player/Player.gd
+++ godot-project/Player/Player.gd
@@ -2,18 +2,24 @@ extends Node2D
 
 const MOTION_SPEED = 8000
 #var JOYPAD_SENSITIVITY = 5
-const JOYPAD_DEADZONE = 0.30
+const JOYPAD_DEADZONE = 0.50
+var joypad_vec
 
-#func _ready():
-#	pass
+func _ready():
+	joypad_vec = Vector2(0,0)
 
 func _physics_process(delta):
-	var joypad_vec = Vector2(0, 0)
-	
-	joypad_vec = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
-	
-	if joypad_vec.length() < JOYPAD_DEADZONE:
-		joypad_vec = Vector2(0, 0)
-	else:
-		joypad_vec = joypad_vec.normalized() * ((joypad_vec.length() - JOYPAD_DEADZONE) / (1 - JOYPAD_DEADZONE))
+	var newjoypad_vec = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
+
+	# Only change the joypad_vec if it's outside the deadzone
+	# no normalization
+	# this means the player can speed up or go back to a slower speed
+	if newjoypad_vec.length() > JOYPAD_DEADZONE:
+		joypad_vec = newjoypad_vec
+
+	# Only change the joypad_vec if it's outside the deadzone
+#	if newjoypad_vec.length() > JOYPAD_DEADZONE:
+		# kinda janky but this code is the reason
+#		joypad_vec = newjoypad_vec.normalized() * ((newjoypad_vec.length() - JOYPAD_DEADZONE) / (1 - JOYPAD_DEADZONE))
+
 	get_node("KinematicBody2D").move_and_slide(joypad_vec*delta*MOTION_SPEED)
