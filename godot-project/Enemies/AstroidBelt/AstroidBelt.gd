45d96f96a2f7cecb1a0053d2e0efae69e36dd94e
Konrad Wisniewski <konrad.m.wisniewski@gmail.com>
2019-01-26 23:30:16 -0700

    Added astroids

---
+++ godot-project/Enemies/AstroidBelt/AstroidBelt.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# class member variables go here, for example:
+
+var rotation_speed = 0.2
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	self.rotation = (self.rotation  + delta * rotation_speed)
