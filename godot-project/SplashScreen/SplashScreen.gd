91bd5ef44c499d467ed4e109013d8191c5200fac
Konrad Wisniewski <konrad.m.wisniewski@gmail.com>
2019-01-27 14:47:14 -0700

    Changes to north south

--- godot-project/SplashScreen/SplashScreen.gd
+++ godot-project/SplashScreen/SplashScreen.gd
@@ -16,10 +16,10 @@ func _ready():
 	get_node("PauseMsg").visible = false
 
 func startScreenVisible(value):
-	get_parent().get_parent().get_node("Obstacle").visible = value
-	get_parent().get_parent().get_node("UI/Node2D").visible = value
-	get_parent().get_parent().get_node("PowerUp4").visible = value
-
+	#get_parent().get_parent().get_node("Obstacle").visible = value
+	#get_parent().get_parent().get_node("UI/Node2D").visible = value
+	#get_parent().get_parent().get_node("PowerUp4").visible = value
+	pass
 func tick():
 	get_node("ButtonsAnim").nextFrame()
 	get_node("KeyboardAnim").nextFrame()
