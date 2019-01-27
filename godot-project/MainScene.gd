27eb16080e8a922c61f1d78c05210ab87e9e8a6f
Rikki Prince <rfp@ecs.soton.ac.uk>
2019-01-26 23:48:30 -0700

    Removing superfluous or clauses on input

--- godot-project/MainScene.gd
+++ godot-project/MainScene.gd
@@ -21,9 +21,9 @@ func setGamePause(pause):
 func _process(delta):
 	# Just a quick helper to close the game for now
 	# Should pause and open a menu in the future
-	if Input.is_key_pressed(KEY_ESCAPE) || Input.is_action_pressed("ui_quit"):
+	if Input.is_action_pressed("ui_quit"):
 		get_tree().quit()
-	if Input.is_key_pressed(KEY_SPACE) || Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("ui_accept"):
 		setGamePause(false)
 	if Input.is_action_pressed("ui_cancel"):
 		setGamePause(true)
