443ebf9c657dacb662915c141cf2706e09bbd905
Lee Davey <lee.davey@gmail.com>
2019-01-27 12:07:52 -0700

    adding splash screen and controlling the pausing and quit from splash screen

--- godot-project/MainScene.gd
+++ godot-project/MainScene.gd
@@ -9,9 +9,10 @@ func _ready():
 func _process(delta):
 	# Just a quick helper to close the game for now
 	# Should pause and open a menu in the future
-	if Input.is_action_pressed("ui_quit"):
-		get_tree().quit()
+#	if Input.is_action_pressed("ui_quit"):
+#		get_tree().quit()
 #	if Input.is_action_pressed("ui_accept"):
 #		setGamePause(false)
 #	if Input.is_action_pressed("ui_cancel"):
 #		setGamePause(true)
+	pass
